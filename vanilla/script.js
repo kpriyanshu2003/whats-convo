ed64137db8ce66a8f1c84452b485fa3e7714caf3
Kumar Priyanshu <az.priyanshu.rockstar@gmail.com>
2024-05-10 01:48:16 +0530

    Frontend Done

---
+++ vanilla/script.js
@@ -0,0 +1,42 @@
+const form = document.getElementById("form");
+const phone = document.getElementById("phone");
+const message = document.getElementById("message");
+const file = document.getElementById("file");
+
+console.log(process.env.VALUE);
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  if (!validate()) return;
+  const res = await submitForm();
+  if (res === true) alert("Message sent successfully");
+  else alert(res);
+});
+
+async function submitForm() {
+  const formData = new FormData();
+  formData.append("phone", phone.value);
+  formData.append("message", message.value);
+  formData.append("file", file.files[0]);
+  const response = await axios("http://localhost:3300/chat", {
+    method: "post",
+    data: formData,
+  });
+  if (response.data.success) return true;
+  else return response.data.message;
+}
+
+function validate() {
+  if (phone.value.trim() === "") {
+    alert("Phone is required");
+    return false;
+  }
+  if (phone.value.trim().length < 10) {
+    alert("Phone number must be 10 digits");
+    return false;
+  }
+  if (file && file.files.length === 0 && message.value.trim() === "") {
+    alert("Message is required");
+    return false;
+  }
+  return true;
+}
